cf0ec96d8632abf6542ec4e771681c045f663a92
Lafsitoo <fretteleandro@live.com>
2022-12-30 20:57:07 -0300

    30/11-20:57

--- script.js
+++ script.js
@@ -3,6 +3,13 @@ let uncoveredCards = 0;
 let cardOne = null;
 let cardTwo = null;
 let resultOne = null;
+let resultTwo = null;
+let moves = 0;
+let correct = 0;
+
+// Archivos HTML
+let uncoveredMoves = document.getElementById("moves");
+let uncoveredCorrects = document.getElementById("correct");
 
 // Generación de numeros
 let numbers = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8];
@@ -18,9 +25,33 @@ function showLetter(id) {
   console.log(uncoveredCards);
 
   if (uncoveredCards == 1) {
-    // Mostrar pimer num
+    // Mostrar primer num
     cardOne = document.getElementById(id);
     resultOne = numbers[id];
     cardOne.innerHTML = resultOne;
+
+    // Deshabilitamos el boton
+    cardOne.disabled = true;
+  } else if (uncoveredCards == 2) {
+    // Mostrar segundo num
+    cardTwo = document.getElementById(id);
+    resultTwo = numbers[id];
+    cardTwo.innerHTML = resultTwo;
+
+    // Tambien deshabilitamos el baton
+    cardTwo.disabled = true;
+
+    // Sumamos +1 a los movimientos
+    moves++;
+    uncoveredMoves.innerHTML = `Moves: ${moves}`;
+
+    if (resultOne == resultTwo) {
+      // Seteamos
+      uncoveredCards = 0;
+
+      // +1 Aciertos
+      correct++;
+      uncoveredCorrects.innerHTML = `Corrects: ${correct}`;
+    }
   }
 }
